fce89dd71d443e93815f2b8af5618e9f629727e7
omtinez <omtinez@gmail.com>
2016-01-18 22:39:20 -0800

    Bump version

--- pddb/__init__.py
+++ pddb/__init__.py
@@ -4,6 +4,6 @@ from .pddb import PandasDatabase
 
 __author__ = 'Oscar Martinez'
 __email__ = 'omtinez@gmail.com'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 __all__ = ['PandasDatabase', 'tostr']
